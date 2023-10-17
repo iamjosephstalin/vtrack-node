01c228e1f6ed94c3bd90512605216bbcbfa8e26b
joseph stalin <joseph.anaiot@gmail.com>
2023-10-17 11:22:42 +0530

    pagination added

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,46 @@
 const express = require('express');
-const Model = require('../models/model');
+const roleModel = require('../models/rolemodel');
 
 const router = express.Router()
 
+// Pagination function
+function paginate(model) {
+    return async (req, res, next) => {
+      const page = parseInt(req.query.page);
+      const limit = parseInt(req.query.limit);
+      const startIndex = (page - 1) * limit;
+      const endIndex = page * limit;
+  
+      const result = {};
+    // change model.length to model.countDocuments() because you are counting directly from mongodb
+      if (endIndex < (await model.countDocuments().exec())) {
+        result.next = {
+          page: page + 1,
+          limit: limit,
+        };
+      }
+      if (startIndex > 0) {
+        result.previous = {
+          page: page - 1,
+          limit: limit,
+        };
+      }
+      try {
+  //       .limit(limit).skip(startIndex) replaced the slice method because 
+  //       it is done directly from mongodb and they are one of mongodb methods
+        result.results = await model.find().limit(limit).skip(startIndex);
+        res.paginatedResult = result;
+        next();
+      } catch (e) {
+        res.status(500).json({ message: e.message });
+      }
+    };
+  }
+
+
 //Post Method
 router.post('/post', async (req, res) => {
-    const data = new Model({
+    const data = new roleModel({
         name: req.body.name,
         role: req.body.role
     })
@@ -20,10 +55,10 @@ router.post('/post', async (req, res) => {
 })
 
 //Get all Method
-router.get('/getAll', async (req, res) => {
+router.get('/getRoles', paginate(roleModel), async (req, res) => {
     try{
-        const data = await Model.find();
-        res.json(data)
+        // const data = await Model.find();
+        res.json(res.paginatedResult);
     }
     catch(error){
         res.status(500).json({message: error.message})
@@ -33,7 +68,7 @@ router.get('/getAll', async (req, res) => {
 //Get by ID Method
 router.get('/getOne/:id', async (req, res) => {
     try{
-        const data = await Model.findById(req.params.id);
+        const data = await roleModel.findById(req.params.id);
         res.json(data)
     }
     catch(error){
@@ -48,7 +83,7 @@ router.patch('/update/:id', async (req, res) => {
         const updatedData = req.body;
         const options = { new: true };
 
-        const result = await Model.findByIdAndUpdate(
+        const result = await roleModel.findByIdAndUpdate(
             id, updatedData, options
         )
 
@@ -64,7 +99,7 @@ router.patch('/update/:id', async (req, res) => {
 router.delete('/delete/:id', async (req, res) => {
     try {
         const id = req.params.id;
-        const data = await Model.findByIdAndDelete(id)
+        const data = await roleModel.findByIdAndDelete(id)
         res.send(`Document with ${data.name} has been deleted..`)
     }
     catch (error) {
