ff61371b2e4447fde52122648459141dcccee96a
joseph stalin <joseph.anaiot@gmail.com>
2023-11-09 21:46:50 +0530

    currency edit by id fix

--- routes/currencies.route.js
+++ routes/currencies.route.js
@@ -8,8 +8,8 @@ router.post('/addCurrency', async (req,res) => {
 
     req.body.forEach(async function (arrayItem) {
 
-        const filter = { name: arrayItem.name };
-        const update = { default: arrayItem.default };
+        const filter = { _id: arrayItem.id };
+        const update = { name: arrayItem.name, default: arrayItem.default };
 
         await currencyModel.countDocuments(filter);
 
