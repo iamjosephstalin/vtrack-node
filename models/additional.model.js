5faa220b54009b68cae9263f1b35fa895a3cef8e
joseph stalin <joseph.anaiot@gmail.com>
2023-11-24 12:01:26 +0530

    auto increment value added

--- models/additional.model.js
+++ models/additional.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-sequence')(mongoose);
 
 const AddSchema = new mongoose.Schema({
+    _id: Number,
     assigned: {
         type: String,
         required: [true, "Please select a value!"],
@@ -13,6 +15,9 @@ const AddSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please Enter the name of the field!"],
     }
-})
+},
+{_id : false}
+)
+AddSchema.plugin(autoIncrement);
 
 module.exports = mongoose.model('additional', AddSchema)
